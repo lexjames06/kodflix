21b28851bbec18d676404781ed430f41bd7c2f31
Lex James <Phlex06@Karende-MBP.localdomain>
2020-01-07 16:56:01 +0000

    Add movie title to cover when hovered

--- src/App.js
+++ src/App.js
@@ -14,59 +14,43 @@ function App() {
       <div className="movie-rows">
         <div className="movie-cover">
           <img src={coolRunnings} alt="cool runnings movie cover" />
+          <div className="movie-title">
+            <h3>Cool Runnings (1993)</h3>
+          </div>
         </div>
         <div className="movie-cover">
           <img src={soulPlane} alt="soul plane movie cover" />
+          <div className="movie-title">
+            <h3>Soul Plane (2004)</h3>
+          </div>
         </div>
         <div className="movie-cover">
           <img src={howHigh} alt="how high movie cover" />
+          <div className="movie-title">
+            <h3>How High (2001)</h3>
+          </div>
         </div>
       </div>
       <div className="movie-rows">
         <div className="movie-cover">
-          <img src={boyz} alt="boyz movie cover" />
+          <img src={boyz} alt="boyz n the hood movie cover" />
+          <div className="movie-title">
+            <h3>Boyz N The Hood (1991)</h3>
+          </div>
         </div>
         <div className="movie-cover">
-          <img src={menace} alt="menace movie cover" />
+          <img src={menace} alt="menace to society movie cover" />
+          <div className="movie-title">
+            <h3>Menace II Society (1993)</h3>
+          </div>
         </div>
         <div className="movie-cover">
           <img src={friday} alt="friday movie cover" />
+          <div className="movie-title">
+            <h3>Friday (1995)</h3>
+          </div>
         </div>
       </div>
-
-
-
-
-
-
-      {/* <div className="movie-rows">
-        <img src={boyz}
-          alt="boyz in the hood movie cover"
-          className="movie-cover"
-        />
-        <img src={menace}
-          alt="menace to society movie cover"
-          className="movie-cover"
-        />
-        <img src={friday}
-          alt="friday movie cover"
-          className="movie-cover"
-        />
-      </div>
-      <div className="movie-rows">
-        <img src={coolRunnings}
-          alt="cool runnings movie cover"
-          className="movie-cover"
-        />
-        <img src={soulPlane}
-          alt="soul plane movie cover"
-          className="movie-cover"
-        />
-        <img src={howHigh}
-          alt="how high movie cover"
-          className="movie-cover"
-        />
-      </div> */}
     </div>
   );
 }
