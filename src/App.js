f91b37a1867045ef08231743615a73036dea564c
Lex James <Phlex06@Karende-MBP.localdomain>
2020-01-07 13:19:56 +0000

    Add movie cover image to 100% width

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import coolRunnings from './movie covers/cool runnings.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to Kodflix</h1>
+      <h1>Welcome to Kodflix</h1>
+      <img src={coolRunnings}
+        alt="cool runnings movie cover"
+        className="movie-cover"
+      />
     </div>
   );
 }
