b9dc761b40cba68912ab32c5ceb958722d97c718
Lex James <Phlex06@Karende-MBP.localdomain>
2020-01-07 13:30:31 +0000

    Add more movie covers and display in rows

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import coolRunnings from './movie covers/cool runnings.jpg';
+import soulPlane from './movie covers/soul plane.jpg';
+import howHigh from './movie covers/how high.jpg';
+import boyz from './movie covers/boyz.jpg';
+import menace from './movie covers/menace.jpg';
+import friday from './movie covers/friday.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Kodflix</h1>
-      <img src={coolRunnings}
-        alt="cool runnings movie cover"
-        className="movie-cover"
-      />
+      <div className="movie-rows">
+        <img src={coolRunnings}
+          alt="cool runnings movie cover"
+          className="movie-cover"
+        />
+        <img src={soulPlane}
+          alt="soul plane movie cover"
+          className="movie-cover"
+        />
+        <img src={howHigh}
+          alt="how high movie cover"
+          className="movie-cover"
+        />
+      </div>
+      <div className="movie-rows">
+        <img src={boyz}
+          alt="boyz in the hood movie cover"
+          className="movie-cover"
+        />
+        <img src={menace}
+          alt="menace to society movie cover"
+          className="movie-cover"
+        />
+        <img src={friday}
+          alt="friday movie cover"
+          className="movie-cover"
+        />
+      </div>
     </div>
   );
 }
