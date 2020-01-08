c598ff06bf5da82805e76c131f46d26051c71a8f
Lex James <Phlex06@Karende-MBP.localdomain>
2020-01-08 14:00:26 +0000

    Change background color and cover-title text size

--- src/Movie.js
+++ src/Movie.js
@@ -5,7 +5,7 @@ export default function Movie(props) {
       <div className='movie-cover'>
         <img src={props.cover} alt={`${props.name} movie cover`} />
         <div className="movie-title">
-          <h1>{`${props.name} (${props.year})`}</h1>
+          <h2>{`${props.name} (${props.year})`}</h2>
         </div>
       </div>
     );
