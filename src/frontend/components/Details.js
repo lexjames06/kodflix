fc9bbb195ce3c8067ee306d6af4e8f6377f990b9
Lex James <Phlex06@MacBook-Pro-3.local>
2020-01-26 14:07:09 +0000

    Add synopsis and cover for each individual movie page

--- src/frontend/components/Details.js
+++ src/frontend/components/Details.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import GetCover from './Cover-get.js'
-import Header from './Header'
 import './Details.css';
 
 class Details extends React.Component {
@@ -10,20 +9,13 @@ class Details extends React.Component {
     this.state = {
       movie: '',
       message: `The details about the movie will display here :)`,
-      color: 'grey'
     };
   }
 
   componentDidMount() {
     let movieId = this.props.match.params.movieId;
     let movie = GetCover().find((movie) => movie.id === movieId);
-    this.setState({ movie });
-    setTimeout(() => {
-      this.setState({
-        message: `Coming Soon!`,
-        color: 'red'
-      });
-    }, 3000);
+    this.setState({ movie, message: movie.synopsis });
   }
 
   render() {
@@ -32,14 +24,16 @@ class Details extends React.Component {
     } else {
       return (
         <div>
-          <Header />
-          <div>
-            <h1>{this.state.movie.name}</h1>
+          <div className="synopsis-image">
+            <img src={this.state.movie.synopsisCover} alt="" />
+          </div>
+          <div className="description">
+            <h1 className="movie-title">{this.state.movie.name}</h1>
             <h2 className="year">({this.state.movie.year})</h2>
             <h2 style={{ color: this.state.color }} className="details">{this.state.message}</h2>
           </div>
           <div className="link-div">
-            <Link to="/kodflix" className="link">Back to home page</Link>
+            <Link to="/kodflix" className="link">Back to homepage</Link>
           </div>
         </div>
       );
